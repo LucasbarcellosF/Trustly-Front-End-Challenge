a990c4cf277c772e27b730cc6775179d1fca10ac
LucasbarcellosF <barcelos.lucas@gmail.com>
2020-08-27 20:51:20 -0300

    Basic pages necessities
    Added initial navbar and main page.

--- trustly/src/App.js
+++ trustly/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Navbar from './components/base/navbar'
 
 function App() {
   return (
+    <>
+    <Navbar />
     <div>
       
     </div>
+    </>
   )
 }
 
---
+++ trustly/src/components/base/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+export default function Navbar() {
+
+  return (
+    <div>
+      <nav class="h-10 bg-gray-400">
+        <div class="mx-auto">
+          <div class="flex justify-center">
+            <span className="text-center">Sneakers</span>  
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
